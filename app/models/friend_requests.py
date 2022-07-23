f3232f7899afa31088c55833743161c96e94afb5
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-23 17:43:42 -0400

    Fix relationships

--- app/models/friend_requests.py
+++ app/models/friend_requests.py
@@ -9,8 +9,8 @@ class FriendRequest(db.Model):
     sending_user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     receiving_user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
-    sending_user = db.relationship("User", foreign_keys=[sending_user_id])
-    receiving_user = db.relationship("User", foreign_keys=[receiving_user_id])
+    sending_user = db.relationship("User", foreign_keys=[sending_user_id], back_populates="sent_friend_requests")
+    receiving_user = db.relationship("User", foreign_keys=[receiving_user_id], back_populates="incoming_friend_requests")
 
     def to_dict(self):
         friend_request_dict = {
