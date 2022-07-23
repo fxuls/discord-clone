a72e120b26650c377a27c377d09009ede70b95d7
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-23 18:16:14 -0400

    Change to list instead of variables

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,42 +3,40 @@ from app.models import db, User
 
 # add users seed
 def seed_users():
-    demo = User(
-        username="demo#1234",
-        password="password",
-        bio="I love this website! Feel free to message me :)",
-        banner_color="#6f27b8",
-        profile_image_id=1,
-    )
+    users = [
+        User(
+            username="demo#1234",
+            password="password",
+            bio="I love this website! Feel free to message me :)",
+            banner_color="#6f27b8",
+            profile_image_id=1,
+        ),
+        User(
+            username="SuperUser#1112",
+            password="password",
+            bio="I enjoy playing games! I mostly will be playing games if I'm online",
+            banner_color="#2eb827",
+            profile_image_id=2,
+        ),
+        User(
+            username="Bobbie#9291",
+            password="password",
+            banner_color="#de667a",
+            profile_image_id=3,
+            status_id=4,
+        ),
+        User(
+            username="LazyUser#0000",
+            password="password",
+        ),
+        User(
+            username="TonyDino#2102",
+            password="password",
+            bio="Hey guys!",
+        ),
+    ]
 
-    super = User(
-        username="SuperUser#1112",
-        password="password",
-        bio="I enjoy playing games! I mostly will be playing games if I'm online",
-        banner_color="#2eb827",
-        profile_image_id=2,
-    )
-
-    bobbie = User(
-        username="Bobbie#9291",
-        password="password",
-        banner_color="#de667a",
-        profile_image_id=3,
-        status_id=4,
-    )
-
-    lazy = User(
-        username="LazyUser#0000",
-        password="password",
-    )
-
-    tony = User(
-        username="TonyDino#2102",
-        password="password",
-        bio="Hey guys!",
-    )
-
-    db.session.add_all([demo, super, bobbie, lazy])
+    db.session.add_all(users)
     db.session.commit()
 
 
