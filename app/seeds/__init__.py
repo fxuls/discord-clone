e1f5e7bab6a2656172c38b5e0321d3746a3ebc59
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-23 15:10:17 -0400

    Add new seed commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,20 +1,24 @@
 from flask.cli import AppGroup
+from .statuses import seed_statuses, undo_statuses
+from .images import seed_images, undo_images
 from .users import seed_users, undo_users
 
-# Creates a seed group to hold our commands
-# So we can type `flask seed --help`
+# creates a seed group to hold our commands
+# so we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 
-# Creates the `flask seed all` command
+# create the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    seed_statuses()
+    seed_images()
     seed_users()
-    # Add other seed functions here
 
 
-# Creates the `flask seed undo` command
+# create the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    # Add other undo functions here
+    undo_images()
+    undo_statuses()
