dd34a482f5475e48da140f7f1f47a961405d4d35
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-24 10:30:47 -0400

    Fix generate_url

--- app/models/servers.py
+++ app/models/servers.py
@@ -3,16 +3,17 @@ import string
 
 from .db import db
 
-class Server(db.Model):
-    def generate_url(length = 12):
-        return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(length))
 
+def generate_url(length = 12):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(length))
+
+class Server(db.Model):
     __tablename__ = "servers"
 
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     name = db.Column(db.String(40), nullable=False)
-    invite_url = db.Column(db.String(120), default=generate_url())
+    invite_url = db.Column(db.String(120), default=lambda: generate_url())
     server_image_id = db.Column(db.Integer, db.ForeignKey("images.id", ondelete="SET NULL"))
     public = db.Column(db.Boolean, nullable=False, default=True)
 
