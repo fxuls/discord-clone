de51c112b2cf4b3575c9ea69e551a159d051ce95
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-24 10:35:04 -0400

    Add to_dict()

--- app/models/servers.py
+++ app/models/servers.py
@@ -4,8 +4,11 @@ import string
 from .db import db
 
 
-def generate_url(length = 12):
-    return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(length))
+def generate_url(length=12):
+    return "".join(
+        random.choice(string.ascii_letters + string.digits) for x in range(length)
+    )
+
 
 class Server(db.Model):
     __tablename__ = "servers"
@@ -14,8 +17,25 @@ class Server(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     name = db.Column(db.String(40), nullable=False)
     invite_url = db.Column(db.String(120), default=lambda: generate_url())
-    server_image_id = db.Column(db.Integer, db.ForeignKey("images.id", ondelete="SET NULL"))
+    server_image_id = db.Column(
+        db.Integer, db.ForeignKey("images.id", ondelete="SET NULL")
+    )
     public = db.Column(db.Boolean, nullable=False, default=True)
 
     owner = db.relationship("User", back_populates="owned_servers")
     server_image = db.relationship("Image", lazy="joined")
+
+    def to_dict(self):
+        server_dict = {
+            "id": self.id,
+            "name": self.name,
+            "owner": self.owner.to_dict(),
+            "public": self.public,
+        }
+
+        if self.invite_url is not None:
+            server_dict["invite_url"] = self.invite_url
+        if self.server_image is not None:
+            server_dict["server_image_url"] = self.server_image.url
+
+        return server_dict
