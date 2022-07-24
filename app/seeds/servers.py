7b97b24bfc2be5689f74cacb1f036b2081ec8671
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-24 09:31:20 -0400

    Initial commit

---
+++ app/seeds/servers.py
@@ -0,0 +1,35 @@
+from app.models import db, Server
+
+# add servers seed
+def seed_servers():
+    servers = [
+        Server(
+            owner_id=1,
+            name="Board Gamers",
+            server_image_id=5,
+        ),
+        Server(
+            owner_id=1,
+            name="Bio Study Group",
+            server_image_id=6,
+            public=False,
+        ),
+        Server(
+            owner_id=2,
+            name="Movie watchers",
+            server_image_id=7,
+        ),
+        Server(
+            owner_id=3,
+            name="Pet photos central",
+            server_image_id=8,
+            public=False,
+        ),
+    ]
+
+
+# truncate servers table
+# remove data, reset primary key, cascade delete dependent entries
+def undo_servers():
+    db.session.execute("TRUNCATE servers RESTART IDENTITY CASCADE;")
+    db.session.commit()
