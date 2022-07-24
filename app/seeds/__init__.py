7648531af4b6fe0c5fe98b395cc36e84bd54bf6d
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-24 09:31:27 -0400

    Add image seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -4,6 +4,7 @@ from .images import seed_images, undo_images
 from .users import seed_users, undo_users
 from .friend_requests import seed_friend_requests, undo_friend_requests
 from .friends import seed_friends, undo_friends
+from .servers import seed_servers, undo_servers
 
 # creates a seed group to hold our commands
 # so we can type `flask seed --help`
@@ -18,11 +19,13 @@ def seed():
     seed_users()
     seed_friend_requests()
     seed_friends()
+    seed_servers()
 
 
 # create the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_servers()
     undo_friends()
     undo_friend_requests()
     undo_users()
