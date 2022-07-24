1bcc590be0e2b37f337076da18a5c4633989d6df
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-24 11:06:08 -0400

    Change columns

--- app/models/server_permissions.py
+++ app/models/server_permissions.py
@@ -5,12 +5,14 @@ class ServerPermission(db.Model):
     __tablename__ = "server_permissions"
 
     id = db.Column(db.Integer, primary_key=True)
-    server_permission = db.Column(db.String(15), nullable=False, unique=True)
+    name = db.Column(db.String(15), nullable=False, unique=True)
+    permission = db.Column(db.Integer, nullable=False, unique=True)
 
     def to_dict(self):
         server_permission_dict = {
             "id": self.id,
-            "server_permission": self.server_permission,
+            "name": self.name,
+            "permission": self.permission,
         }
 
         return server_permission_dict
