5986d91c8c6eeccb1864ee68e05fe42fe3dc18d5
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-26 08:57:46 -0400

    Add more seeds

--- app/seeds/server_members.py
+++ app/seeds/server_members.py
@@ -5,12 +5,19 @@ from app.models import db, ServerMember
 def seed_server_members():
     server_members = [
         ServerMember(server_id=1, user_id=1, permission_id=4),
-        ServerMember(server_id=2, user_id=1, permission_id=4),
-        ServerMember(server_id=3, user_id=2, permission_id=4),
-        ServerMember(server_id=4, user_id=3, permission_id=4),
-        ServerMember(server_id=1, user_id=2, permission_id=2),
         ServerMember(server_id=1, user_id=3, permission_id=3),
-        ServerMember(server_id=3, user_id=1, permission_id=1),
+        ServerMember(server_id=1, user_id=2),
+
+        ServerMember(server_id=2, user_id=1, permission_id=4),
+        ServerMember(server_id=2, user_id=4),
+
+        ServerMember(server_id=3, user_id=2, permission_id=4),
+        ServerMember(server_id=3, user_id=3, permission_id=1),
+        ServerMember(server_id=3, user_id=1),
+
+        ServerMember(server_id=4, user_id=3, permission_id=4),
+        ServerMember(server_id=4, user_id=4, permission_id=3),
+        ServerMember(server_id=4, user_id=5),
     ]
 
     db.session.add_all(server_members)
