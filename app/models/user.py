245679b78cbe425278d02e4f084ed30bbe8d21b9
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-27 20:53:59 -0400

    Add email column

--- app/models/user.py
+++ app/models/user.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     bio = db.Column(db.String(320))
     banner_color = db.Column(db.String(10))
