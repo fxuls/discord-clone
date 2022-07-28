24fa8014881b4204901cff613d31ba2b2a402f11
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-27 20:53:16 -0400

    Rename to SignInForm

--- app/forms/signin_form.py
+++ app/forms/signin_form.py
@@ -23,7 +23,7 @@ def password_matches(form, field):
         raise ValidationError('Password was incorrect.')
 
 
-class LoginForm(FlaskForm):
+class SignInForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[
                            DataRequired(), password_matches])
