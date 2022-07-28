c1f6e7573d5c8090112ee2372cbacff8c3427c02
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-28 18:17:51 -0400

    Update fields

--- app/forms/signin_form.py
+++ app/forms/signin_form.py
@@ -2,28 +2,28 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
+from sqlalchemy import func
 
 
 def user_exists(form, field):
     # Checking if user with provided email exists
-    email = field.data.lower()
-    user = User.query.filter(User.email == email).first()
+    email = field.data
+    user = User.query.filter(func.lower(User.email) == email.lower()).first()
     if not user:
-        raise ValidationError('Email provided not found.')
+        raise ValidationError("Email not found")
 
 
 def password_matches(form, field):
     # Checking if password matches
     password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
+    email = form.data["email"]
+    user = User.query.filter(func.lower(User.email) == email.lower()).first()
     if not user:
-        raise ValidationError('No such user exists.')
+        raise ValidationError("User does not exist")
     if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+        raise ValidationError("Password is incorrect")
 
 
 class SignInForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
-    password = StringField('password', validators=[
-                           DataRequired(), password_matches])
+    email = StringField("email", validators=[DataRequired(), Email(), user_exists])
+    password = StringField("password", validators=[DataRequired(), password_matches])
