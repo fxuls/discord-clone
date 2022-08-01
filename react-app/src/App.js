ca2868e95906edd9ee265f76f382b69ba3ce3ad4
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-01 17:47:28 -0400

    Move unauthenticated stuff to its own component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { authenticate, userSelector } from "./store/session";
-import HomePage from "./components/HomePage";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authenticate } from "./store/session";
 import AuthenticatedApp from "./components/AuthentciatedApp";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UnauthenticatedApp from "./components/UnauthenticatedApp";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
-
-  const sessionUser = useSelector(userSelector);
 
   // use effect for intial load
   useEffect(() => {
@@ -29,13 +27,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact={true}>
-          <HomePage />
-        </Route>
-
         <ProtectedRoute path="/app">
           <AuthenticatedApp />
         </ProtectedRoute>
+
+        <UnauthenticatedApp />
       </Switch>
     </BrowserRouter>
   );
