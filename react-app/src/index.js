c178668dc8e307c8e0d30f992576ee2b972e270b
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-01 18:57:03 -0400

    Update component name

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./App";
+import Root from "./Root";
 import configureStore from "./store";
+import "./styles/reset.css";
 import "./styles/index.css";
+import "./styles/navigation.css";
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Root />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
