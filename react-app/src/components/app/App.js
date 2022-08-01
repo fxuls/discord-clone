a50797e54d352d9258d2c4abd5874a63fb4f4721
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-01 18:57:22 -0400

    Rename to App

---
+++ react-app/src/components/app/App.js
@@ -0,0 +1,10 @@
+import SignOutButton from "../auth/SignOutButton"
+
+const App = () => {
+    return <div>
+        <SignOutButton />
+        You are signed in
+    </div>
+}
+
+export default App
