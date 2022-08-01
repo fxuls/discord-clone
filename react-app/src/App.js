00bc3f64cd1534fc32fcfee336f8b7caa78bebd1
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-01 17:17:15 -0400

    Change HTML structure add loading session

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,48 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import SignInForm from './components/auth/SignInForm';
-import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import { authenticate } from './store/session';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { authenticate, userSelector } from "./store/session";
+import HomePage from "./components/HomePage";
+import AuthenticatedApp from "./components/AuthentciatedApp";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async() => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
+  const sessionUser = useSelector(userSelector);
 
-  if (!loaded) {
-    return null;
-  }
+  // use effect for intial load
+  useEffect(() => {
+    if (!loaded)
+      (async () => {
+        try {
+          await dispatch(authenticate());
+        } finally {
+          setLoaded(true);
+        }
+      })();
+  }, [dispatch, loaded]);
+
+  if (!loaded) return null;
 
   return (
     <BrowserRouter>
-      <NavBar />
       <Switch>
-        <Route path='/sign-in' exact={true}>
-          <SignInForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+        {sessionUser ? <AuthenticatedApp /> : <HomePage />}
       </Switch>
     </BrowserRouter>
   );
