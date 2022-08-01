1ea9d5e51d98c61dbcc5078063a1602a51b36e34
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-01 17:30:30 -0400

    Add links

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,24 +1,35 @@
-import { Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
+import { userSelector } from "../store/session";
 import NavBar from "./NavBar";
 import SignInForm from "./auth/SignInForm";
 import SignUpForm from "./auth/SignUpForm";
+import { useEffect } from "react";
 
 const HomePage = () => {
-    return (<div>
-        <NavBar />
+  const user = useSelector(userSelector);
 
-        <Route path="/sign-in" exact={true}>
-          <SignInForm />
-        </Route>
+  useEffect(() => {
+    if (user) return <Redirect to="/" />
+  }, [user])
+  
+  return (
+    <div>
+      <NavBar />
 
-        <Route path="/sign-up" exact={true}>
-          <SignUpForm />
-        </Route>
+      <Route path="/sign-in" exact={true}>
+        <SignInForm />
+      </Route>
 
-        <Route path="/" exact={true}>
-            Homepage
-        </Route>
-    </div>)
-}
+      <Route path="/sign-up" exact={true}>
+        <SignUpForm />
+      </Route>
+
+      <Route path="/" exact={true}>
+        Homepage
+      </Route>
+    </div>
+  );
+};
 
 export default HomePage;
