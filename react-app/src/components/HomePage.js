34657dc1edd5774afe1c94db6b1418383dffa45e
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-01 17:49:52 -0400

    Fix content

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -2,32 +2,12 @@ import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
 import { userSelector } from "../store/session";
 import NavBar from "./NavBar";
-import SignInForm from "./auth/SignInForm";
-import SignUpForm from "./auth/SignUpForm";
 import { useEffect } from "react";
 
 const HomePage = () => {
-  const user = useSelector(userSelector);
-
-  useEffect(() => {
-    if (user) return <Redirect to="/" />
-  }, [user])
-  
   return (
     <div>
-      <NavBar />
-
-      <Route path="/sign-in" exact={true}>
-        <SignInForm />
-      </Route>
-
-      <Route path="/sign-up" exact={true}>
-        <SignUpForm />
-      </Route>
-
-      <Route path="/" exact={true}>
-        Homepage
-      </Route>
+        Hi! This is the Discord homepage
     </div>
   );
 };
