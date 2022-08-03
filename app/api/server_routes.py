3c8b2e69f4fca6c91fd191e8a58b97f7c6b1c59f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-02 20:19:34 -0400

    Remove print

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -7,5 +7,4 @@ server_routes = Blueprint("servers", __name__)
 @server_routes.route("/joined")
 @login_required
 def get_joined_servers():
-    print(current_user.joined_servers)
     return jsonify(current_user.joined_servers), 200
