b03e9c334ae4c4e5e3de44f0d45629ec10630302
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-02 20:24:03 -0400

    Add get server by id route

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -7,4 +7,20 @@ server_routes = Blueprint("servers", __name__)
 @server_routes.route("/joined")
 @login_required
 def get_joined_servers():
+    """
+    Get a list of all the server memberships the current user
+    belongs to
+
+    Each result includes the server_id and the permission_id of
+    the users permission in the server
+    """
     return jsonify(current_user.joined_servers), 200
+
+
+@server_routes.route("/<int:id>")
+def get_server_by_id(id):
+    """
+    Get a server's details by its id
+    """
+    server = Server.query.get(id)
+    return server.to_dict()
