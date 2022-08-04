9796015995e16f302fc400baef5ba88d7ea03ea3
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-03 20:17:52 -0400

    Add extra lines

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -103,8 +103,10 @@ export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_USER:
       return { user: action.payload };
+
     case REMOVE_USER:
       return { user: null };
+      
     default:
       return state;
   }
