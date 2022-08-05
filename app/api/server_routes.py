bc42393378f975d3801bf6b1d873adbaeef181e3
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-04 20:02:42 -0400

    Add more joining and leaving routes

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -4,7 +4,33 @@ from app.models import db, User, Server, ServerMember
 
 server_routes = Blueprint("servers", __name__)
 
-@server_routes.route("/joined")
+SERVER_NOT_FOUND = {
+    "message": "Server not found",
+    "status_code": 404,
+}
+
+USER_IS_BANNED = {
+    "message": "User is banned",
+    "status_code": 401,
+}
+
+SERVER_IS_PRIVATE = {
+    "message": "Server is private",
+    "status_code": 401,
+}
+
+USER_ALREADY_MEMBER = {
+    "message": "User is already a member",
+    "status_code": 400,
+}
+
+USER_NOT_MEMBER = {
+    "message": "User is not a member",
+    "status_code": 400,
+}
+
+
+@server_routes.route("/joined", methods=["GET"])
 @login_required
 def get_joined_servers():
     """
@@ -17,7 +43,7 @@ def get_joined_servers():
     return jsonify(current_user.joined_servers), 200
 
 
-@server_routes.route("/<int:id>")
+@server_routes.route("/<int:id>", methods=["GET"])
 def get_server_by_id(id):
     """
     Get a server's details by its id
@@ -26,10 +52,11 @@ def get_server_by_id(id):
 
     if server:
         return jsonify(server.to_dict())
-    return jsonify(None), 404
+    return jsonify(SERVER_NOT_FOUND), 404
 
 
 @server_routes.route("/<int:id>/memberships", methods=["POST"])
+@login_required
 def join_server_by_id(id):
     """
     Attempt to join a server by its id
@@ -41,36 +68,23 @@ def join_server_by_id(id):
 
     # if server does not exist
     if server is None:
-        return jsonify({
-            "message": "Server not found",
-            "status_code": 404,
-        }), 404
+        return jsonify(SERVER_NOT_FOUND), 404
 
     # check if user is already a member
 
     membership = ServerMember.query.filter(ServerMember.user_id == current_user.id).filter(ServerMember.server_id == id).first()
-    print(membership)
-    if (membership is not None):
+    if membership is not None:
         # check if user is banned
         permission = membership.permission.name
         if permission == "banned":
-            return jsonify({
-                "message": "User is banned",
-                "status_code": 401,
-            }), 401
+            return jsonify(USER_IS_BANNED), 401
 
         # user is already a member error
-        return jsonify({
-            "message": "User is already a member",
-            "status_code": 400,
-        }), 400
+        return jsonify(USER_ALREADY_MEMBER), 400
 
     # check if server is public
     if not server.public:
-        return jsonify({
-            "message": "Server is private",
-            "status_code": 401,
-        }), 401
+        return jsonify(SERVER_IS_PRIVATE), 401
 
     # create user membership
     new_membership = ServerMember(server_id=id, user_id=current_user.id)
@@ -81,9 +95,63 @@ def join_server_by_id(id):
     return new_membership.to_dict(), 201
 
 
-@server_routes.route("/join/<path:invite_url", method=["GET"])
+@server_routes.route("/join/<path:invite_url>", methods=["GET", "POST"])
+@login_required
 def join_server_by_url(invite_url):
     """
     Attempt to join a server from its invite url
-
     """
+    server = Server.query.filter(Server.invite_url == invite_url)
+
+    # if server does not exist
+    if server is None:
+        return jsonify(SERVER_NOT_FOUND), 404
+
+    membership = ServerMember.query.filter(ServerMember.user_id == current_user.id).filter(ServerMember.server_id == server.id).first()
+    if membership is not None:
+        # check if user is banned
+        permission = membership.permission.name
+        if permission == "banned":
+            return jsonify(USER_IS_BANNED), 401
+
+        # user is already a member error
+        return jsonify(USER_ALREADY_MEMBER), 400
+
+    # create user membership
+    new_membership = ServerMember(server_id=server.id, user_id=current_user.id)
+
+    db.session.add(new_membership)
+    db.session.commit()
+
+    return new_membership.to_dict(), 201
+
+
+@server_routes.route("/<int:id>/memberships", methods=["DELETE"])
+@login_required
+def leave_server(id):
+    """
+    Leave a server by its id
+    """
+    server = Server.query.get(id)
+
+    # if server does not exist
+    if server is None:
+        return jsonify(SERVER_NOT_FOUND), 404
+
+    # check user is member
+    membership = ServerMember.query.filter(ServerMember.user_id == current_user.id).filter(ServerMember.server_id == id).first()
+    if membership is None:
+        return jsonify(USER_NOT_MEMBER), 400
+
+    # if user is banned do not remove entry
+    if membership.permission.name == "banned":
+        return jsonify(USER_IS_BANNED), 401
+
+    # delete membership and return success
+    db.session.delete(membership)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Successfully left server",
+        "status_code": 200,
+    }), 200
