1e514a8d8f45cc8fca3e071db63cc567a0f9d986
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-06 15:53:22 -0400

    Initial commit

---
+++ react-app/src/store/ui.js
@@ -0,0 +1,34 @@
+export const SET_SERVER = "ui/SHOW_SERVER";
+export const CLEAR_SERVER = "ui/CLEAR_SERVER";
+
+// SET_SERVER action creator
+export const setServer = (serverId) => ({
+    type: SET_SERVER,
+    payload: serverId,
+});
+
+// CLEAR_SERVER action creator
+export const clearServer = () => ({
+    type: CLEAR_SERVER,
+});
+
+const initialState = { serverId: null };
+
+export default function uiReducer(state = initialState, action) {
+    const newState = { ...state };
+
+    switch (action.type) {
+        case SET_SERVER:
+            newState.serverId = action.payload;
+            break;
+
+        case CLEAR_SERVER:
+            newState.serverId = null;
+            break;
+
+        default:
+            break;
+    }
+
+    return newState;
+}
