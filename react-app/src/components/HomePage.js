63818ca40204fa2ea2df7c4db25091d1c4351498
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-06 13:35:30 -0400

    I dont know

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,7 +1,21 @@
+import { useDispatch } from "react-redux";
+import {
+  fetchServer,
+  fetchJoinedServers,
+  removeServer,
+  joinServerById,
+  joinServerByUrl,
+  leaveServer,
+  fetchAllServers,
+} from "../store/servers";
+
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const onClick = (e) => dispatch(fetchServer(1));
   return (
     <div>
-        Hi! This is the Discord homepage
+      Hi! This is the Discord homepage
     </div>
   );
 };
