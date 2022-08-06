99b4730580e8b4dbaefabd4e056744570a230aa4
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-06 13:35:09 -0400

    Add fetching data on load

--- react-app/src/components/app/App.js
+++ react-app/src/components/app/App.js
@@ -1,10 +1,31 @@
-import SignOutButton from "../auth/SignOutButton"
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchJoinedServers } from "../../store/servers";
+
+import SignOutButton from "../auth/SignOutButton";
+import ServerList from "./servers/ServerList";
 
 const App = () => {
-    return <div>
-        <SignOutButton />
-        You are signed in
-    </div>
-}
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
 
-export default App
+  useEffect(() => {
+    if (!loaded)
+      (async () => {
+        await dispatch(fetchJoinedServers());
+        setLoaded(true);
+      })();
+  }, [dispatch, loaded]);
+
+  if (!loaded) return <div className="app">Loading...</div>;
+
+  return (
+    <div className="app">
+        <ServerList />
+      <SignOutButton />
+      You are signed in
+    </div>
+  );
+};
+
+export default App;
