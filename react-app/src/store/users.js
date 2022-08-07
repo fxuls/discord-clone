62c4dc63bd2131031068af7503ecba1971ef4056
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-07 14:58:17 -0400

    Add fetchUser thunk

--- react-app/src/store/users.js
+++ react-app/src/store/users.js
@@ -1,8 +1,14 @@
+export const SET_USERS = "users/SET_USERS";
 export const SET_USER = "users/SET_USER";
 export const REMOVE_USER = "users/REMOVE_USER";
 
 // selector
 
+// SET_USERS action creator
+export const setUsers = (users) => ({
+    type: SET_USERS,
+    payload: users,
+});
 
 // SET_USER action creator
 export const setUser = (user) => ({
@@ -16,7 +22,20 @@ export const removeUser = (userId) => ({
     payload: userId,
 });
 
-const initialState = {};
+// fetch user by id thunk
+export const fetchUser = (userId) => async (dispatch) => {
+    const response = await fetch(`/api/users/${userId}`, {});
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(setUser(data));
+        return true;
+    }
+
+    return false;
+}
+
+const initialState = { friends: null };
 
 export default function usersReducer(state = initialState, action) {
     const newState = { ...state };
