60425eaa67834d897fea84bcc1b7a899e81302d2
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-08 17:49:43 -0400

    Add current direct message chat open

--- react-app/src/store/ui.js
+++ react-app/src/store/ui.js
@@ -1,8 +1,11 @@
 export const SET_SERVER = "ui/SHOW_SERVER";
 export const CLEAR_SERVER = "ui/CLEAR_SERVER";
+export const SET_DIRECT_MESSAGE_ID = "ui/SET_DIRECT_MESSAGE";
+export const CLEAR_DIRECT_MESSAGE_ID = "ui/CLEAR_DIRECT_MESSAGE";
 
 // selectors
 export const uiServerIdSelector = (state) => state.ui.serverId;
+export const uiDirectMessageIdSelector = (state) => state.ui.directMessageId;
 
 // SET_SERVER action creator
 export const setServer = (serverId) => ({
@@ -15,7 +18,18 @@ export const clearServer = () => ({
     type: CLEAR_SERVER,
 });
 
-const initialState = { serverId: null };
+// SET_DIRECT_MESSAGE_ID action creator
+export const setDirectMessageId = (directMessageId) => ({
+    type: SET_DIRECT_MESSAGE_ID,
+    payload: directMessageId,
+});
+
+// CLEAR_DIRECT_MESSAGE_ID action creator
+export const clearDirectMessageId = () => ({
+    type: CLEAR_DIRECT_MESSAGE_ID,
+});
+
+const initialState = { serverId: null, directMessageId: null };
 
 export default function uiReducer(state = initialState, action) {
     const newState = { ...state };
@@ -28,6 +42,13 @@ export default function uiReducer(state = initialState, action) {
         case CLEAR_SERVER:
             newState.serverId = null;
             break;
+
+        case SET_DIRECT_MESSAGE_ID:
+            newState.directMessageId = action.payload;
+            break;
+
+        case CLEAR_DIRECT_MESSAGE_ID:
+            newState.directMessageId = null;
 
         default:
             break;
