83d7289d2af20da4b42b6dfb61c7c8d350ce0e4a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-08 18:18:25 -0400

    Add route to get current users friends

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import User
 
 user_routes = Blueprint('users', __name__)
@@ -10,14 +10,14 @@ USER_NOT_FOUND = {
 }
 
 
-@user_routes.route('/')
+@user_routes.route("/")
 @login_required
 def users():
     users = [user.to_dict() for user in User.query.all()]
     return jsonify(users)
 
 
-@user_routes.route('/<int:id>')
+@user_routes.route("/<int:id>")
 @login_required
 def user(id):
     user = User.query.get(id)
@@ -25,3 +25,9 @@ def user(id):
     if user:
         return user.to_dict()
     return jsonify(USER_NOT_FOUND), 404
+
+
+@user_routes.route("/friends")
+@login_required
+def get_friends():
+    return jsonify(current_user.friends), 200
