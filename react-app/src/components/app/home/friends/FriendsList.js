38f0e22977d67602b594965910830a03a0bd2e99
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 18:43:07 -0400

    Initial commit

---
+++ react-app/src/components/app/home/friends/FriendsList.js
@@ -0,0 +1,20 @@
+import FriendCard from "./FriendCard";
+
+const FriendsList = ({ friends }) => {
+  return (
+    <div className="friends header-box-shadow">
+      <h1 className="sub-header-text">{`Friends — ${friends.length}`}</h1>
+
+      <ul className="friends-list">
+        {friends.map((user) => (
+          <li key={user.id}>
+            <div className="list-seperator" />
+            <FriendCard user={user} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FriendsList;
