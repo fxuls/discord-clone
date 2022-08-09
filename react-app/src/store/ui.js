b8e217f7ee9799e467fba8e7f068bcbb130c286f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 17:37:21 -0400

    Add friend tab state

--- react-app/src/store/ui.js
+++ react-app/src/store/ui.js
@@ -2,10 +2,15 @@ export const SET_SERVER = "ui/SHOW_SERVER";
 export const CLEAR_SERVER = "ui/CLEAR_SERVER";
 export const SET_DIRECT_MESSAGE_ID = "ui/SET_DIRECT_MESSAGE";
 export const CLEAR_DIRECT_MESSAGE_ID = "ui/CLEAR_DIRECT_MESSAGE";
+export const SET_FRIENDS_TAB = "ui/SET_FRIENDS_TAB";
+
+export const FRIENDS_TAB_ALL = "ui/FRIENDS_TAB_ALL";
+export const FRIENDS_TAB_PENDING = "ui/FRIENDS_TAB_PENDING";
 
 // selectors
 export const uiServerIdSelector = (state) => state.ui.serverId;
 export const uiDirectMessageIdSelector = (state) => state.ui.directMessageId;
+export const uiFriendsTabSelector = (state) => state.ui.friendsTab;
 
 // SET_SERVER action creator
 export const setServer = (serverId) => ({
@@ -29,14 +34,21 @@ export const clearDirectMessageId = () => ({
     type: CLEAR_DIRECT_MESSAGE_ID,
 });
 
-const initialState = { serverId: null, directMessageId: null };
+// SET_FRIENDS_TAB action creator
+export const setFriendsTab = (tab) => ({
+    type: SET_FRIENDS_TAB,
+    payload: tab,
+});
+
+const initialState = { serverId: null, directMessageId: null, friendsTab: FRIENDS_TAB_ALL };
 
 export default function uiReducer(state = initialState, action) {
     const newState = { ...state };
+    const payload = action.payload;
 
     switch (action.type) {
         case SET_SERVER:
-            newState.serverId = action.payload;
+            newState.serverId = payload;
             break;
 
         case CLEAR_SERVER:
@@ -44,11 +56,14 @@ export default function uiReducer(state = initialState, action) {
             break;
 
         case SET_DIRECT_MESSAGE_ID:
-            newState.directMessageId = action.payload;
+            newState.directMessageId = payload;
             break;
 
         case CLEAR_DIRECT_MESSAGE_ID:
             newState.directMessageId = null;
+
+        case SET_FRIENDS_TAB:
+            newState.friendsTab = payload;
 
         default:
             break;
