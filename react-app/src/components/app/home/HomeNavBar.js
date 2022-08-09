0a455bf6ab9ddbd6ebe708c177ae31a5abaaa189
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-08 20:57:39 -0400

    Fix class name

--- react-app/src/components/app/home/HomeNavBar.js
+++ react-app/src/components/app/home/HomeNavBar.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
 const HomeNavBar = ({ currentDirectMessageId }) => {
-    return <div className="nav-bar box-shadow-right">
+    return <div className="nav-bar left-inset-shadow">
         <div className="nav-item unselectable" id="friends-button" active={(currentDirectMessageId === null) + ""}><FontAwesomeIcon className="icon" icon={faUsers} /><h1>Friends</h1></div>
         <h2 className="unselectable">Direct messages</h2>
     </div>
