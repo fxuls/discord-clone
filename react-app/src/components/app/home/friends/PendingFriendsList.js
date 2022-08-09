302d103553c1c8c845fa15a18e4f7f49dd29b02b
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 19:44:43 -0400

    Make headers unselectable

--- react-app/src/components/app/home/friends/PendingFriendsList.js
+++ react-app/src/components/app/home/friends/PendingFriendsList.js
@@ -5,22 +5,22 @@ import FriendCard from "./FriendCard";
 const PendingFriendsList = ({ friendRequests }) => {
   return (
     <div className="friends header-box-shadow">
-      <h1 className="sub-header-text">{`Incoming — ${friendRequests.incoming.length}`}</h1>
+      <h1 className="sub-header-text unselectable">{`Incoming — ${friendRequests.incoming.length}`}</h1>
       <ul className="friends-list incoming">
         {friendRequests.incoming.map((userId) => (
           <li key={userId}>
             <div className="list-seperator" />
-            <FriendCard userId={userId} type="incoming"/>
+            <FriendCard userId={userId} type="incoming" />
           </li>
         ))}
       </ul>
 
-      <h1 className="sub-header-text ">{`Sent — ${friendRequests.sent.length}`}</h1>
+      <h1 className="sub-header-text unselectable">{`Sent — ${friendRequests.sent.length}`}</h1>
       <ul className="friends-list incoming">
         {friendRequests.sent.map((userId) => (
           <li key={userId}>
             <div className="list-seperator" />
-            <FriendCard userId={userId} type="sent"/>
+            <FriendCard userId={userId} type="sent" />
           </li>
         ))}
       </ul>
