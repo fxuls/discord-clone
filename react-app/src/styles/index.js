db07396b7f30ee1105ce4e8752f148aa21939a5f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-08 23:28:47 -0400

    Add colors and getting random colors

--- react-app/src/styles/index.js
+++ react-app/src/styles/index.js
@@ -7,3 +7,15 @@ import "./app.css";
 import "./nav-bars.css";
 import "./friends.css";
 import "./home.css";
+
+export const colors = [
+  "#5865F2", // Blurple
+  "#02c39a", // Green
+  "#f7b801", // Yellow
+  "#EB459E", // Fuscsia
+  "#ED4245", // Red
+  "#7209b7", // Purple
+];
+
+export const getRandomColor = () =>
+  colors[Math.floor(Math.random() * colors.length)];
