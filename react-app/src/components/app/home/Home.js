6736b449eb57f2dc22f8160256bf33eb802a17ab
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-08 22:46:38 -0400

    Add loading data and rendering component

--- react-app/src/components/app/home/Home.js
+++ react-app/src/components/app/home/Home.js
@@ -1,5 +1,31 @@
-const Home = () => {
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFriends } from "../../../store/users";
+import { uiDirectMessageIdSelector } from "../../../store/ui";
+import HomeNavBar from "./HomeNavBar";
+import Friends from "./friends/Friends";
 
-}
+const Home = () => {
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+
+  const currentDirectMessageId = useSelector(uiDirectMessageIdSelector);
+
+  useEffect(() => {
+    if (!loaded)
+      (async () => {
+        await dispatch(fetchFriends());
+        setLoaded(true);
+      })();
+  }, [dispatch, loaded]);
+
+  return (
+    <div className="home content">
+      <HomeNavBar currentDirectMessageId={currentDirectMessageId}/>
+
+      {currentDirectMessageId ? "" : <Friends loaded={loaded}/>}
+    </div>
+  );
+};
 
 export default Home;
