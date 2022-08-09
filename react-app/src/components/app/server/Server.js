315e4dc26553fdd281a5bc11b5fa7a5e7681e02d
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-08 23:41:03 -0400

    Initial commit

--- react-app/src/components/app/server/Server.js
+++ react-app/src/components/app/server/Server.js
@@ -1,5 +1,11 @@
-const Server = () => {
+import ServerNavBar from "./ServerNavBar";
 
-}
+const Server = () => {
+  return (
+    <div>
+      <ServerNavBar />
+    </div>
+  );
+};
 
 export default Server;
