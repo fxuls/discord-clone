a716787d5476c671dfb2e8ae0be46583eebc6c77
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 17:05:34 -0400

    Add friends button functionality

--- react-app/src/components/app/home/HomeNavBar.js
+++ react-app/src/components/app/home/HomeNavBar.js
@@ -1,11 +1,25 @@
+import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { setDirectMessageId } from "../../../store/ui";
 
 const HomeNavBar = ({ currentDirectMessageId }) => {
-    return <div className="nav-bar left-inset-shadow">
-        <div className="nav-item unselectable" id="friends-button" active={(currentDirectMessageId === null) + ""}><FontAwesomeIcon className="icon" icon={faUsers} /><h1>Friends</h1></div>
-        <h2 className="unselectable sub-header-text">Direct messages</h2>
+  const dispatch = useDispatch();
+
+  return (
+    <div className="nav-bar left-inset-shadow">
+      <div
+        className="nav-item unselectable"
+        id="friends-button"
+        active={(currentDirectMessageId === null) + ""}
+        onClick={() => dispatch(setDirectMessageId(null))}
+      >
+        <FontAwesomeIcon className="icon" icon={faUsers} />
+        <h1>Friends</h1>
+      </div>
+      <h2 className="unselectable sub-header-text">Direct messages</h2>
     </div>
-}
+  );
+};
 
 export default HomeNavBar;
