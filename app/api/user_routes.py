fdb6e85006295520f3b4022590174806594bb1a9
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 17:01:11 -0400

    Add remove friend route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-from app.models import User
+from app.models import db, User, Friend
 
 user_routes = Blueprint('users', __name__)
 
 USER_NOT_FOUND = {
     "message": "Server not found",
     "status_code": 404,
+}
+
+USERS_NOT_FRIENDS = {
+    "message": "Users are not friends",
+    "status_code": 400,
 }
 
 
@@ -31,3 +36,26 @@ def user(id):
 @login_required
 def get_friends():
     return jsonify(current_user.friends), 200
+
+
+@user_routes.route("/friends/<int:id>", methods=["DELETE"])
+@login_required
+def remove_friend(id):
+    """
+    Remove a friend
+    """
+    # find the friend obj
+    friendship = Friend.query.filter(Friend.user_one_id == current_user.id, Friend.user_two_id == id).first()
+    if friendship is None:
+        friendship = Friend.query.filter(Friend.user_one_id == id, Friend.user_two_id == current_user.id).first()
+
+    if friendship is None:
+        return jsonify(USERS_NOT_FRIENDS), 400
+
+    db.session.delete(friendship)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Successfully unfriended user",
+        "status": 200,
+    }), 200
