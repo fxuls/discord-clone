b22e1c26744491224a00af312cac95da4488d5b1
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 17:37:05 -0400

    Add tab buttons

--- react-app/src/components/app/home/friends/FriendHeader.js
+++ react-app/src/components/app/home/friends/FriendHeader.js
@@ -1,7 +1,43 @@
-const FriendHeader = () => {
-    return <div className="friends-header">
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
+import {
+  setFriendsTab,
+  FRIENDS_TAB_ALL,
+  FRIENDS_TAB_PENDING,
+  uiFriendsTabSelector,
+} from "../../../../store/ui";
+
+const FriendHeader = () => {
+  const dispatch = useDispatch();
+
+  const currentTab = useSelector(uiFriendsTabSelector);
+
+  return (
+    <div className="friends-header">
+      <h1>
+        <FontAwesomeIcon icon={faUsers} className="icon" />
+        <span>Friends</span>
+      </h1>
+
+      <button
+        className="text-button"
+        active={(currentTab === FRIENDS_TAB_ALL) + ""}
+        onClick={() => dispatch(setFriendsTab(FRIENDS_TAB_ALL))}
+      >
+        All
+      </button>
+
+      <button
+        className="text-button"
+        active={(currentTab === FRIENDS_TAB_PENDING) + ""}
+        onClick={() => dispatch(setFriendsTab(FRIENDS_TAB_PENDING))}
+      >
+        Pending
+      </button>
     </div>
-}
+  );
+};
 
 export default FriendHeader;
