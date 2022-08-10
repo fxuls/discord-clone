e0235295db6932828e067ec035e2d5541e932223
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 16:53:56 -0400

    Add card content

--- react-app/src/components/app/home/messages/MessageCard.js
+++ react-app/src/components/app/home/messages/MessageCard.js
@@ -2,21 +2,36 @@ import { useSelector } from "react-redux";
 import { userSelector } from "../../../../store/users";
 
 const MessageCard = ({ message }) => {
-    const sender = useSelector(userSelector(message.sender_id));
+  const sender = useSelector(userSelector(message.sender_id));
+  const name = sender.username.split("#")[0];
 
-    return <div className="message-card">
+  return (
+    <div className="message-card">
+      <div className="message-icon-container">
         {sender.profile_image_url ? (
-        <img className="user-icon message-icon" src={sender.profile_image_url} />
-      ) : (
-        <div
-          className="default-image-container message-icon"
-          style={{ backgroundColor: sender.color }}
-        >
-          <img className="user-icon message-icon" src="/assets/default-user.png" />
-        </div>
-      )}
-        {message.text}
-        </div>;
-}
+          <img
+            className="user-icon message-icon"
+            src={sender.profile_image_url}
+          />
+        ) : (
+          <div className="default-image-container"
+            style={{ backgroundColor: sender.color }}
+          >
+            <img
+              className="message-icon user-icon"
+              src="/assets/default-user.png"
+            />
+          </div>
+        )}
+      </div>
+
+      <h1>{name}</h1>
+
+      <div className="message-content">
+        <p>{message.text}</p>
+      </div>
+    </div>
+  );
+};
 
 export default MessageCard;
