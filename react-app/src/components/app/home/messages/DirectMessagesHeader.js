ab83a6cadd67c9906570e8515f830bc66f761b4b
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 15:16:21 -0400

    Move selecting to props from parent

--- react-app/src/components/app/home/messages/DirectMessagesHeader.js
+++ react-app/src/components/app/home/messages/DirectMessagesHeader.js
@@ -5,11 +5,8 @@ import { uiDirectMessageIdSelector } from "../../../../store/ui";
 import { userSelector } from "../../../../store/users";
 import { directMessageChatSelector } from "../../../../store/directMessages";
 
-const DirectMessagesHeader = () => {
-    const uiDirectMessageId = useSelector(uiDirectMessageIdSelector);
-    const chat = useSelector (directMessageChatSelector(uiDirectMessageId))
-    const user = useSelector(userSelector(chat.userId));
-    const name = user.username.split("#")[0]
+const DirectMessagesHeader = ({ user }) => {
+    const name = user.username.split("#")[0];
 
     return <div className="header transparent-caret-color">
         <h1 className="unselectable">
