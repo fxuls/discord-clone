27e81429a578819341f9de36198c74fcc4bc45cc
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-09 21:03:03 -0400

    Add add friend tab

--- react-app/src/components/app/home/friends/Friends.js
+++ react-app/src/components/app/home/friends/Friends.js
@@ -7,11 +7,14 @@ import {
 import {
   uiFriendsTabSelector,
   FRIENDS_TAB_ALL,
+  FRIENDS_TAB_PENDING,
+  FRIENDS_TAB_ADD,
 } from "../../../../store/ui";
 
 import FriendHeader from "./FriendHeader";
 import FriendsList from "./FriendsList";
 import PendingFriendsList from "./PendingFriendsList";
+import AddFriend from "./AddFriend";
 
 const Friends = ({ loaded }) => {
   const friends = useSelector(friendsSelector);
@@ -26,11 +29,9 @@ const Friends = ({ loaded }) => {
   return (
     <div className="friends-container main left-inset-shadow">
       <FriendHeader />
-        {tab === FRIENDS_TAB_ALL ? (
-          <FriendsList friends={friends} />
-        ) : (
-          <PendingFriendsList friendRequests={friendRequests} />
-        )}
+        {tab === FRIENDS_TAB_ALL && <FriendsList friends={friends} />}
+        {tab === FRIENDS_TAB_PENDING && <PendingFriendsList friendRequests={friendRequests} />}
+        {tab === FRIENDS_TAB_ADD && <AddFriend />}
     </div>
   );
 };
