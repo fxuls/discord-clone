08bc809ecfc6a75ae137091b2cec7ce4dafe8439
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 03:23:20 -0400

    Add rendering cards of each direct message chat

--- react-app/src/components/app/home/HomeNavBar.js
+++ react-app/src/components/app/home/HomeNavBar.js
@@ -1,10 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { setDirectMessageId } from "../../../store/ui";
+import { directMessagesIdsSelector } from "../../../store/directMessages";
+import DirectMessageChatCard from "./directMessages/DirectMessageChatCard";
 
 const HomeNavBar = ({ currentDirectMessageId }) => {
   const dispatch = useDispatch();
+  const directMessageChatIds = useSelector(directMessagesIdsSelector);
+
+  // rerender on change in directMessageChatIds
+  useEffect(() => {}, [directMessageChatIds]);
 
   return (
     <div className="nav-bar left-inset-shadow">
@@ -17,7 +24,12 @@ const HomeNavBar = ({ currentDirectMessageId }) => {
         <FontAwesomeIcon className="icon" icon={faUsers} />
         <h1>Friends</h1>
       </div>
+
       <h2 className="unselectable sub-header-text">Direct messages</h2>
+
+      <ul>
+        {directMessageChatIds.map((userId) => <li key={userId}><DirectMessageChatCard userId={userId}/></li>)}
+      </ul>
     </div>
   );
 };
