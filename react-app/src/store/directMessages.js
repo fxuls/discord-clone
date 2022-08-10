a46cd556b44d70dd0b7073ae0bc926a1d2f89a5f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 02:53:52 -0400

    Add fetch direct messages thunk

--- react-app/src/store/directMessages.js
+++ react-app/src/store/directMessages.js
@@ -1,9 +1,44 @@
+import { fetchUser } from "./users";
+
 const SET_DIRECT_MESSAGES = "messages/SET_DIRECT_MESSAGES";
 
 // selectors
 
 // SET_DIRECT_MESSAGES action creator
-export const setDirectMessages = (messages) => ({
-    type: SET_DIRECT_MESSAGES,
-    payload: messages,
+export const setDirectMessages = (directMessages) => ({
+  type: SET_DIRECT_MESSAGES,
+  payload: directMessages,
 });
+
+export const fetchDirectMessages = () => async (dispatch) => {
+  const response = await fetch("/api/direct-messages", {});
+
+  if (response.ok) {
+    const directMessages = await response.json();
+
+    dispatch(setDirectMessages(directMessages));
+
+    // fetch the users who have chats
+    Object.keys(directMessages).forEach((userId) =>
+      dispatch(fetchUser(userId))
+    );
+  }
+};
+
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+  let newState = { ...state };
+  const payload = action.payload;
+
+  switch (action.type) {
+    case SET_DIRECT_MESSAGES:
+      newState = payload;
+      break;
+
+    default:
+      break;
+  }
+
+  return newState;
+}
