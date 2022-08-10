27ab165f66262cf1e25193019798f0cc987844e9
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 14:30:17 -0400

    Add direct chat cards

--- react-app/src/components/app/home/directMessages/DirectMessageChatCard.js
+++ react-app/src/components/app/home/directMessages/DirectMessageChatCard.js
@@ -1,30 +1,48 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { userSelector } from "../../../../store/users";
 import { setDirectMessageId } from "../../../../store/ui";
+import { directMessageChatSelector } from "../../../../store/directMessages";
 
-const DirectMessageChatCard = ({ userId, active}) => {
-    const dispatch = useDispatch();
-    const user = useSelector(userSelector(userId));
-    const name = user.username.split("#")[0];
+const DirectMessageChatCard = ({ directMessageChatId, active }) => {
+  const dispatch = useDispatch();
+  const chat = useSelector(directMessageChatSelector(directMessageChatId));
+  const user = useSelector(userSelector(chat.userId));
+  const name = user.username.split("#")[0];
 
-    // rerender on change in user
-    useEffect(() => {}, [user]);
+  // rerender on change in user
+  useEffect(() => {}, [user]);
 
-    return <div className="nav-item direct-message-chat-card" active={active + ""} onClick={() => dispatch(setDirectMessageId(userId))}>
-        {user.profile_image_url ? (
-          <img className="user-icon nav-icon" src={user.profile_image_url} />
-        ) : (
-          <div
-            className="default-image-container nav-icon"
-            style={{ backgroundColor: user.color }}
-          >
-            <img className="user-icon nav-icon" src="/assets/default-user.png" />
-          </div>
-        )}
+  const onDeleteDirectChat = () => dispatch();
 
-        <h1>{name}</h1>
+  return (
+    <div
+      className="nav-item direct-message-chat-card"
+      active={active + ""}
+      onClick={() => dispatch(setDirectMessageId(directMessageChatId))}
+    >
+      {user.profile_image_url ? (
+        <img className="user-icon nav-icon" src={user.profile_image_url} />
+      ) : (
+        <div
+          className="default-image-container nav-icon"
+          style={{ backgroundColor: user.color }}
+        >
+          <img className="user-icon nav-icon" src="/assets/default-user.png" />
+        </div>
+      )}
+
+      <h1>{name}</h1>
+
+      <FontAwesomeIcon
+        className="delete-chat-icon transparent-caret-color"
+        icon={faXmark}
+        onClick={onDeleteDirectChat}
+      />
     </div>
-}
+  );
+};
 
 export default DirectMessageChatCard;
