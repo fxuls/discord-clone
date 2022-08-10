6d9f7ac2ef3588811531af0d350d986a3be2508a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 19:49:55 -0400

    Add contents

--- react-app/src/components/modals/ImageModal.js
+++ react-app/src/components/modals/ImageModal.js
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 export const IMAGE_MODAL = "modals/IMAGE_MODAL";
 
-const ImageModal = () => {
-    return <div>Image Modal</div>;
-}
+const ImageModal = ({ modal, closeModal, stopPropagation }) => {
+  const { imageUrl } = modal;
+
+  return (
+    <div className="image-modal">
+      <div className="image-container">
+        <img src={imageUrl} onMouseDown={stopPropagation} />
+        <a
+          href={imageUrl}
+          target="_blank"
+          className="transparent-caret-color"
+          onMouseDown={stopPropagation}
+        >
+          Open original
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default ImageModal;
