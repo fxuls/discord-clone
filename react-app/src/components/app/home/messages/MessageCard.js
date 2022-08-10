1c8c313d131895bde12d0d1fb1faf6d309f479c6
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 15:44:14 -0400

    Add user icon

--- react-app/src/components/app/home/messages/MessageCard.js
+++ react-app/src/components/app/home/messages/MessageCard.js
@@ -1,5 +1,22 @@
-const MessageCard = () => {
-    return <div className="message-card">The message will be here</div>;
+import { useSelector } from "react-redux";
+import { userSelector } from "../../../../store/users";
+
+const MessageCard = ({ message }) => {
+    const sender = useSelector(userSelector(message.sender_id));
+
+    return <div className="message-card">
+        {sender.profile_image_url ? (
+        <img className="user-icon message-icon" src={sender.profile_image_url} />
+      ) : (
+        <div
+          className="default-image-container message-icon"
+          style={{ backgroundColor: sender.color }}
+        >
+          <img className="user-icon message-icon" src="/assets/default-user.png" />
+        </div>
+      )}
+        {message.text}
+        </div>;
 }
 
 export default MessageCard;
