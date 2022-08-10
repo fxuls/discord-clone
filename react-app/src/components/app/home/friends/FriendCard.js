a7ef9f5a207d2c07cc136ef897358c33d5722b1c
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 12:20:29 -0400

    Split classes on icons

--- react-app/src/components/app/home/friends/FriendCard.js
+++ react-app/src/components/app/home/friends/FriendCard.js
@@ -61,13 +61,13 @@ const FriendCard = ({ userId, type }) => {
     <div className="friend-card">
       <div className="unselectable">
         {user.profile_image_url ? (
-          <img className="friend-icon" src={user.profile_image_url} />
+          <img className="friend-icon user-icon" src={user.profile_image_url} />
         ) : (
           <div
             className="default-image-container"
             style={{ backgroundColor: user.color }}
           >
-            <img className="friend-icon" src="/assets/default-user.png" />
+            <img className="friend-icon user-icon" src="/assets/default-user.png" />
           </div>
         )}
       </div>
