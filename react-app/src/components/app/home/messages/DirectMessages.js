d0662a636d85b64447ce83a55464465dc2635354
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 15:16:32 -0400

    Feed props into header

--- react-app/src/components/app/home/messages/DirectMessages.js
+++ react-app/src/components/app/home/messages/DirectMessages.js
@@ -1,17 +1,21 @@
 import { useSelector } from "react-redux";
 import { uiDirectMessageIdSelector } from "../../../../store/ui";
+import { directMessageChatSelector, directMessagesSelector } from "../../../../store/directMessages";
+import { userSelector } from "../../../../store/users";
 
 import DirectMessagesHeader from "./DirectMessagesHeader";
 import DirectMessageChatBox from "./DirectMessageChatBox";
 
 const DirectMessages = ({ loaded }) => {
   const uiDirectMessageId = useSelector(uiDirectMessageIdSelector);
-  
+  const chat = useSelector(directMessageChatSelector(uiDirectMessageId));
+  const user = useSelector(userSelector(chat.userId));
+
   if (!loaded) return null;
 
   return (
     <div className="direct-messages-container main left-inset-shadow">
-      <DirectMessagesHeader />
+      <DirectMessagesHeader user={user} />
 
       <div className="direct-messages header-box-shadow">
         Direct messages here
