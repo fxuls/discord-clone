53f2b5134dbd2716e2336209d6998bf0fe6f2c9d
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-10 21:40:42 -0400

    Add a chatbox

--- react-app/src/components/app/home/messages/DirectMessageChatBox.js
+++ react-app/src/components/app/home/messages/DirectMessageChatBox.js
@@ -1,7 +1,29 @@
-const DirectMessageChatBox = () => {
-    return <div className="chat-box">
-        Chatbox
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+const DirectMessageChatBox = ({ userName }) => {
+  const [messageText, setMessageText] = useState("");
+
+  const onSendMessage = (e) => {
+    e.preventDefault();
+
+    console.log("Submit ", messageText);
+  };
+  return (
+    <div className="chat-box">
+      <form className="send-message-form" onSubmit={onSendMessage} autoComplete="off">
+        <input
+          type="text"
+          name="messageText"
+          value={messageText}
+          placeholder={`Message @${userName}`}
+          onChange={(e) => setMessageText(e.target.value)}
+        />
+        <input type="submit" style={{ display: "none" }} />
+      </form>
     </div>
-}
+  );
+};
 
 export default DirectMessageChatBox;
