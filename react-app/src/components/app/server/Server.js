c64982d6900666c6e4198e7d48973cec003216e6
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-11 19:29:42 -0400

    Prop thread open channel id

--- react-app/src/components/app/server/Server.js
+++ react-app/src/components/app/server/Server.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { uiServerChannelSelector, uiServerIdSelector } from "../../../store/ui";
-import { fetchServerChannels, joinedServersIdsSelector, serverSelector, joinedServersSelector } from "../../../store/servers";
+import { fetchServerChannels, serverSelector, } from "../../../store/servers";
 
 import ServerNavBar from "./ServerNavBar";
 import ServerMessages from "./ServerMessages";
@@ -25,7 +25,7 @@ const Server = () => {
 
   return (
     <div className="server content">
-      <ServerNavBar loaded={loaded} server={server} />
+      <ServerNavBar loaded={loaded} server={server} uiServerChannelId={uiServerChannelId}/>
 
       <ServerMessages loaded={loaded} server={server}/>
     </div>
