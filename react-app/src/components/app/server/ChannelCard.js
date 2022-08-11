c2c5ba2d0afeb5611734471dd978d509d8b5e1d8
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-11 19:44:03 -0400

    Add active data tag

--- react-app/src/components/app/server/ChannelCard.js
+++ react-app/src/components/app/server/ChannelCard.js
@@ -3,15 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 import { setServerChannelId } from "../../../store/ui";
 
-const ChannelCard = ({ channel }) => {
+const ChannelCard = ({ channel, active }) => {
   const dispatch = useDispatch();
   const onOpenChannel = () =>
     dispatch(setServerChannelId(channel.server_id, channel.id));
-    
+
   return (
     <div
       onClick={onOpenChannel}
       className="channel-card nav-item unselectable transparent-caret-color"
+      active={active + ""}
     >
       <FontAwesomeIcon icon={faHashtag} className="hashtag-icon" />
       <span className="channel-name">{channel.name}</span>
