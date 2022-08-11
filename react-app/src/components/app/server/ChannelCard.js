4a3c827ee9996d201633dc65ce940620a900d2b9
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-11 19:27:37 -0400

    Add content

--- react-app/src/components/app/server/ChannelCard.js
+++ react-app/src/components/app/server/ChannelCard.js
@@ -1,11 +1,22 @@
+import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { setServerChannelId } from "../../../store/ui";
 
 const ChannelCard = ({ channel }) => {
-    return <div className="channel-card nav-item unselectable transparent-caret-color">
-        <FontAwesomeIcon icon={faHashtag} className="hashtag-icon" />
-        <span className="channel-name">{channel.name}</span>
+  const dispatch = useDispatch();
+  const onOpenChannel = () =>
+    dispatch(setServerChannelId(channel.server_id, channel.id));
+    
+  return (
+    <div
+      onClick={onOpenChannel}
+      className="channel-card nav-item unselectable transparent-caret-color"
+    >
+      <FontAwesomeIcon icon={faHashtag} className="hashtag-icon" />
+      <span className="channel-name">{channel.name}</span>
     </div>
-}
+  );
+};
 
 export default ChannelCard;
