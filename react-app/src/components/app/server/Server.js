d18d8981b935e2c565a0ccbcd316395bb9687533
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-11 16:36:53 -0400

    Add components and loading resources structure

--- react-app/src/components/app/server/Server.js
+++ react-app/src/components/app/server/Server.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { uiServerIdSelector } from "../../../store/ui";
+
 import ServerNavBar from "./ServerNavBar";
+import ServerMessages from "./ServerMessages";
 
 const Server = () => {
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+  const uiServerId = useSelector(uiServerIdSelector);
+
+  useEffect(() => {
+    if (!loaded)
+      (async () => {
+        console.log("Fetch server resouces");
+        setLoaded(true);
+      })();
+  }, [dispatch, loaded]);
+
   return (
     <div>
-      <ServerNavBar />
+      <ServerNavBar loaded={loaded} />
+
+      <ServerMessages loaded={loaded} />
     </div>
   );
 };
