451d70e79c880de88dcfbf4b05aa1e47de05a544
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-11 16:56:10 -0400

    Remove unused code

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,23 +1,5 @@
-import { useDispatch } from "react-redux";
-import {
-  fetchServer,
-  fetchJoinedServers,
-  removeServer,
-  joinServerById,
-  joinServerByUrl,
-  leaveServer,
-  fetchAllServers,
-} from "../store/servers";
-
 const HomePage = () => {
-  const dispatch = useDispatch();
-
-  const onClick = (e) => dispatch(fetchServer(1));
-  return (
-    <div>
-      Hi! This is the Discord homepage
-    </div>
-  );
+  return <div>Hi! This is the Discord homepage</div>;
 };
 
 export default HomePage;
