daf671f56f6cf76699abcc38f1432f72ba0adbdf
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-12 15:55:50 -0400

    Add add server message route

--- app/api/server_message_routes.py
+++ app/api/server_message_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.api.server_routes import USER_IS_BANNED
-from app.models import db, ServerMessage, Server
+from app.api.server_routes import USER_IS_BANNED, USER_NOT_MEMBER
+from app.models import db, ServerMessage, Server, ServerMember
 
 server_message_routes = Blueprint("server-messages", __name__)
 
@@ -10,11 +10,15 @@ SERVER_NOT_EXIST = {
     "status_code": 404,
 }
 
-USER_IS_BANNED = {
-    "message": "User is banned",
-    "status_code": 401,
+CHANNEL_ID_REQUIRED = {
+    "message": "channel_id is required",
+    "status_code": 400,
 }
 
+CONTENT_MISSING = {
+    "message": "Content missing",
+    "status_code": 400,
+}
 
 @server_message_routes.route("/servers/<int:id>/messages", methods=["GET"])
 @login_required
@@ -33,3 +37,50 @@ def get_server_messages(id):
         return jsonify(USER_IS_BANNED), 401
 
     return jsonify([message.to_dict() for message in server.messages]), 200
+
+
+
+@server_message_routes.route("/servers/<int:id>/messages", methods=["POST"])
+@login_required
+def post_message_to_server(id):
+    """
+    Sends a new message to a server channel
+    """
+    # check that server exists
+    server = Server.query.get(id)
+    if server is None:
+        return jsonify(SERVER_NOT_EXIST), 404
+
+    body = request.get_json()
+
+    # if channel_id not provided
+    if "channel_id" not in body:
+        return jsonify(CHANNEL_ID_REQUIRED), 400
+
+    # if message content is missing
+    if "text" not in body and "image_id" not in body:
+        return jsonify(CONTENT_MISSING), 400
+
+    # check if user is a member of server
+    membership = ServerMember.query.filter(ServerMember.server_id == id, ServerMember.user_id == current_user.id).first()
+    if membership is None:
+        return jsonify(USER_NOT_MEMBER)
+
+    # check that user is not banned
+    if membership.permission.name == "banned":
+        return jsonify(USER_IS_BANNED), 401
+
+    # create message
+    message = ServerMessage(
+        sender_id=current_user.id,
+        server_id=id,
+        channel_id=body["channel_id"],
+        text=(body["text"] if "text" in body else None),
+        image_id=(body["image_id"] if "image_id" in body else None),
+    )
+
+    # add the message to db and return it
+    db.session.add(message)
+    db.session.commit()
+
+    return jsonify(message.to_dict()), 201
