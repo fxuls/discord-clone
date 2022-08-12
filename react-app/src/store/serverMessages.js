35789bad6499f40d3ef9fea62a0420e3d2d65d10
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-11 20:26:36 -0400

    Initial commit

---
+++ react-app/src/store/serverMessages.js
@@ -0,0 +1,40 @@
+const SET_SERVER_MESSAGES = "messages/SET_SERVER_MESSAGES";
+
+// SET_SERVER_MESSAGES action creator
+export const setServerMessages = (serverId, serverMessages) => ({
+  type: SET_SERVER_MESSAGES,
+  payload: {
+    serverId,
+    serverMessages,
+  },
+});
+
+// fetch server messages thunk
+export const fetchServerMessages = (serverId) => async (dispatch) => {
+  const response = await fetch(
+    `/api/server-messages/servers/${serverId}/messages`,
+    {}
+  );
+
+  const data = await response.json();
+  if (response.ok) dispatch(setServerMessages(serverId, data));
+  return data;
+};
+
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+  let newState = { ...state };
+  const payload = action.payload;
+
+  switch (action.type) {
+    case SET_SERVER_MESSAGES:
+      newState[payload.serverId] = payload.serverMessages;
+      break;
+
+    default:
+      break;
+  }
+
+  return newState;
+}
