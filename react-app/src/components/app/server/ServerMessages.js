3091f27e8735915bf94546d8b193c3e952bfbd6c
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-12 14:05:08 -0400

    Add messages

--- react-app/src/components/app/server/ServerMessages.js
+++ react-app/src/components/app/server/ServerMessages.js
@@ -1,7 +1,27 @@
-const ServerMessages = ({ loaded }) => {
-    return <div className="server-messages">
+import { useSelector } from "react-redux";
+import {
+  serverMessagesSelector,
+  serverChannelMessagesSelector,
+} from "../../../store/serverMessages";
 
-    </div>;
-}
+import MessageCard from "../home/messages/MessageCard";
+
+const ServerMessages = ({ loaded, server, channelId }) => {
+  const messages = useSelector(
+    serverChannelMessagesSelector(server.id, channelId)
+  );
+
+  if (!loaded || !messages?.length) return <div>No messages here yet...</div>;
+
+  return (
+    <div className="server-messages">
+      {messages.map((message) => (
+        <li key={message.id}>
+          <MessageCard message={message} />
+        </li>
+      ))}
+    </div>
+  );
+};
 
 export default ServerMessages;
