865f291ce29e6f8195257daa5af30a229afb9b83
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-13 00:24:24 -0400

    Add permission selector

--- react-app/src/components/app/server/Server.js
+++ react-app/src/components/app/server/Server.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { uiServerChannelSelector, uiServerIdSelector } from "../../../store/ui";
-import { fetchServerChannels, serverSelector, } from "../../../store/servers";
+import {
+  fetchServerChannels,
+  serverSelector,
+  currentUserServerPermissionSelector,
+} from "../../../store/servers";
 import { fetchServerMessages } from "../../../store/serverMessages";
 
 import ServerNavBar from "./ServerNavBar";
@@ -12,29 +16,43 @@ const Server = () => {
   const [loaded, setLoaded] = useState(false);
   const uiServerId = useSelector(uiServerIdSelector);
   const server = useSelector(serverSelector(uiServerId));
+  const permission = useSelector(
+    currentUserServerPermissionSelector(server.id)
+  );
 
   let uiServerChannelId = useSelector(uiServerChannelSelector(uiServerId));
   // if no channel click set use the first channel
   if (loaded && server?.channels && !uiServerChannelId)
     uiServerChannelId = Object.values(server.channels)[0].id;
 
-  useEffect(() => setLoaded(false), [uiServerId])
+  useEffect(() => setLoaded(false), [uiServerId]);
 
   useEffect(() => {
     if (!loaded)
       (async () => {
-        const results = [];
-        results.push(await dispatch(fetchServerChannels(uiServerId)));
-        results.push(await dispatch(fetchServerMessages(uiServerId)));
+        const results = [
+          await dispatch(fetchServerChannels(uiServerId)),
+          await dispatch(fetchServerMessages(uiServerId)),
+        ];
+
         setLoaded(true);
+        return results;
       })();
   }, [dispatch, loaded, uiServerId, server]);
 
   return (
     <div className="server content">
-      <ServerNavBar loaded={loaded} server={server} activeChannelId={uiServerChannelId}/>
+      <ServerNavBar
+        loaded={loaded}
+        server={server}
+        activeChannelId={uiServerChannelId}
+      />
 
-      <ServerMessages loaded={loaded} server={server} channelId={uiServerChannelId} />
+      <ServerMessages
+        loaded={loaded}
+        server={server}
+        channelId={uiServerChannelId}
+      />
     </div>
   );
 };
