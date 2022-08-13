367a82cec01d5f1233cf809f6f666ca6c31b6e54
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-13 13:38:32 -0400

    Get logged in user state

--- react-app/src/components/app/App.js
+++ react-app/src/components/app/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchJoinedServers } from "../../store/servers";
+import { fetchUser } from "../../store/users";
+import { currentUserIdSelector } from "../../store/session";
 import { uiServerIdSelector } from "../../store/ui";
 
 import ServerList from "./serversList/ServerList";
@@ -11,14 +13,16 @@ const App = () => {
   const dispatch = useDispatch();
   const uiServerId = useSelector(uiServerIdSelector);
   const [loaded, setLoaded] = useState(false);
+  const currentUserId = useSelector(currentUserIdSelector);
 
   useEffect(() => {
     if (!loaded)
       (async () => {
         await dispatch(fetchJoinedServers());
+        await dispatch(fetchUser(currentUserId));
         setLoaded(true);
       })();
-  }, [dispatch, loaded, uiServerId]);
+  }, [dispatch, loaded, uiServerId, currentUserId]);
 
   if (!loaded) return <div className="app">Loading...</div>;
 
