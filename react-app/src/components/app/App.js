99e54228483ea441c9eff72175c789ecced2b03b
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-13 19:13:44 -0400

    Add sockets

--- react-app/src/components/app/App.js
+++ react-app/src/components/app/App.js
@@ -1,15 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchJoinedServers } from "../../store/servers";
 import { fetchUser } from "../../store/users";
 import { currentUserIdSelector } from "../../store/session";
 import { uiServerIdSelector } from "../../store/ui";
+import { fetchDirectMessages } from "../../store/directMessages";
 
+import { SocketContext } from "../sockets";
 import ServerList from "./serversList/ServerList";
 import Home from "./home/Home";
 import Server from "./server/Server";
 
 const App = () => {
+  const socket = useContext(SocketContext);
   const dispatch = useDispatch();
   const uiServerId = useSelector(uiServerIdSelector);
   const [loaded, setLoaded] = useState(false);
@@ -21,6 +24,10 @@ const App = () => {
         await dispatch(fetchJoinedServers());
         await dispatch(fetchUser(currentUserId));
         setLoaded(true);
+
+        socket.on("NEW_DIRECT_MESSAGE", (data) => {
+          dispatch(fetchDirectMessages());
+        });
       })();
   }, [dispatch, loaded, uiServerId, currentUserId]);
 
