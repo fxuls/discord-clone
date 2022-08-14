27cd8574908cc628916c221cd8096629ccef24db
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-14 14:20:03 -0400

    Add update server socket listener

--- react-app/src/components/app/App.js
+++ react-app/src/components/app/App.js
@@ -8,6 +8,7 @@ import {
   directMessageChatIdsSelector,
   fetchDirectChat,
 } from "../../store/directMessages";
+import { fetchServerMessages } from "../../store/serverMessages";
 
 import { SocketContext } from "../sockets";
 import ServerList from "./serversList/ServerList";
@@ -32,7 +33,12 @@ const App = () => {
         socket.on("UPDATE_DIRECT_MESSAGE_CHAT", async (data) => {
           console.log("UPDATE_DIRECT_MESSAGE_CHAT", data, directMessageChatIds);
           // if (directMessageChatIds.includes(data.chat_id + ""))
-            await dispatch(fetchDirectChat(data.chat_id));
+          await dispatch(fetchDirectChat(data.chat_id));
+        });
+
+        socket.on("UPDATE_SERVER_MESSAGES", async (data) => {
+          console.log("UPDATE_SERVER_MESSAGES", data);
+          await dispatch(fetchServerMessages(data.server_id));
         });
       })();
   }, [dispatch, loaded, uiServerId, currentUserId]);
