b64a6ca7ad0b22f0126d860efbd36fe41a445429
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-14 17:47:07 -0400

    Add basic content

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,5 +1,21 @@
+import NavBar from "./navigation/NavBar";
+
 const HomePage = () => {
-  return <div>Hi! This is the Discord homepage</div>;
+  return (
+    <div className="homepage-container">
+      <NavBar />
+      <div className="homepage-header">
+        <h1>Imagine a place...</h1>
+        <p>
+          ...where you can belong to a school club, a gaming group, or a
+          worldwide art community. Where just you and a handful of friends can
+          spend time together. A place that makes it easy to talk every day and
+          hang out more often.
+        </p>
+      </div>
+      The servers to join will go here
+    </div>
+  );
 };
 
 export default HomePage;
