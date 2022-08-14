d66461113db898be38b3d3641a6f723af7bcde20
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-14 18:22:20 -0400

    Add background color wrapper

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -3,18 +3,20 @@ import NavBar from "./navigation/NavBar";
 const HomePage = () => {
   return (
     <div className="homepage-container">
-      <NavBar />
-      <div className="homepage-header">
-        <h1>Imagine a place...</h1>
-        <p>
-          ...where you can belong to a school club, a gaming group, or a
-          worldwide art community. Where just you and a handful of friends can
-          spend time together. A place that makes it easy to talk every day and
-          hang out more often.
-        </p>
+      <div className="bg-color-box">
+        <NavBar />
+        <div className="homepage-header">
+          <h1>Imagine a place...</h1>
+          <p>
+            ...where you can belong to a school club, a gaming group, or a
+            worldwide art community. Where just you and a handful of friends can
+            spend time together. A place that makes it easy to talk every day
+            and hang out more often.
+          </p>
+        </div>
       </div>
 
-      <div className="homepage-servers fill-height header-box-shadow">
+      <div className="homepage-servers header-box-shadow">
         The servers to join will go here
       </div>
     </div>
