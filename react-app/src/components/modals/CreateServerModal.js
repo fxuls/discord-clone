9ab49a68376233e375bd1e607e9b398e83850aef
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-15 09:33:28 -0400

    Add CreateServerForm

--- react-app/src/components/modals/CreateServerModal.js
+++ react-app/src/components/modals/CreateServerModal.js
@@ -1,8 +1,10 @@
+import CreateServerForm from "../app/server/CreateServerForm";
+
 export const CREATE_SERVER_MODAL = "modals/CREATE_SERVER_MODAL";
 
 const CreateServerModal = () => {
-    return (<div className="modal-form-container">
-        Create server modal
+    return (<div className="form-modal form-container">
+        <CreateServerForm />
     </div>);
 }
 
