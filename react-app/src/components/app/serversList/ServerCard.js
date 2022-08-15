1a266ea50bcece95bb7319592cbce778d78c12b1
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-15 00:22:26 -0400

    Add initials to server without picture

--- react-app/src/components/app/serversList/ServerCard.js
+++ react-app/src/components/app/serversList/ServerCard.js
@@ -3,23 +3,31 @@ import { setServer } from "../../../store/ui";
 
 const ServerCard = ({ server, active }) => {
   const dispatch = useDispatch();
+  const initials = server.name.split(" ").map(word => word[0]);
 
   const openServer = () => dispatch(setServer(server.id));
 
   return (
-    <div className="server-card">
-      <img
-        onClick={openServer}
-        alt="Server icon"
-        className="server-icon unselectable"
-        active={active + ""}
-        src={server.server_image_url}
-      />
+    <div
+      className={server.server_image_url ? "server-card" : "home-card"}
+      active={active + ""}
+    >
+      {server.server_image_url ? (
+        <img
+          onClick={openServer}
+          alt="Server icon"
+          className="server-icon unselectable"
+          active={active + ""}
+          src={server.server_image_url}
+        />
+      ) : (
+        <h1 className="server-icon" onClick={openServer} active={active + ""}>
+          {initials.slice(0, 2).join("")}
+        </h1>
+      )}
       <div className="server-info-modal">
         <div className="left-arrow"></div>
-        <div className="server-info">
-          {server.name}
-        </div>
+        <div className="server-info">{server.name}</div>
       </div>
     </div>
   );
