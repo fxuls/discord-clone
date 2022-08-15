ef05064738e081ea14662d89fe23cc6ed9a878f9
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-14 20:46:38 -0400

    Add redirect if authenticated

--- react-app/src/components/auth/SignInPage.js
+++ react-app/src/components/auth/SignInPage.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { sessionUserSelector } from "../../store/session";
 import NavBar from "../navigation/NavBar";
 import SignInForm from "./SignInForm";
 
 const SignInPage = () => {
+  const sessionUser = useSelector(sessionUserSelector);
+
+  if (sessionUser) return <Redirect to="/" />
+
   return (
     <div className="form-page">
       <NavBar />
