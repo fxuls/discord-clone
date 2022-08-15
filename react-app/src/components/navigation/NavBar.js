078baf18a0ada7a5f0843c14b0a0d39e3046bcf2
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-14 20:46:24 -0400

    Rename selector

--- react-app/src/components/navigation/NavBar.js
+++ react-app/src/components/navigation/NavBar.js
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { userSelector } from "../../store/session";
+import { sessionUserSelector } from "../../store/session";
 import AuthenticatedNav from "./AuthenticatedNav";
 import UnauthenticatedNav from "./UnauthenticatedNav";
 
 const NavBar = () => {
   const history = useHistory();
-  const sessionUser = useSelector(userSelector);
+  const sessionUser = useSelector(sessionUserSelector);
 
   return (
     <nav>
