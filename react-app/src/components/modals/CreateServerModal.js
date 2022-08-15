126e180b6580ab5af213d20d5616cde5e4f725d3
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-14 23:01:23 -0400

    Initial commit

---
+++ react-app/src/components/modals/CreateServerModal.js
@@ -0,0 +1,9 @@
+export const CREATE_SERVER_MODAL = "modals/CREATE_SERVER_MODAL";
+
+const CreateServerModal = () => {
+    return (<div>
+        Create server modal
+    </div>);
+}
+
+export default CreateServerModal;
