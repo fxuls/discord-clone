0819709fa981f5c38551b9f4e71ac20eaa80c79d
fxuls <32501694+fxuls@users.noreply.github.com>
2022-08-15 20:36:21 -0400

    Fix photos maybe

--- app/__init__.py
+++ app/__init__.py
@@ -73,8 +73,10 @@ def inject_csrf_token(response):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path == 'favicon.svg':
+        return app.send_static_file('favicon.svg')
+    if path[0:6] == 'assets':
+        return app.send_static_file(path[7:])
     return app.send_static_file('index.html')
 
 
